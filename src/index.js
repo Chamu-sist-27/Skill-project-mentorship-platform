42d63db65f143091fb9207988debc0673ddb9dac
chamu-sist-27 <chamusist27@gmail.com>
2025-12-28 12:01:43 +0530

    added admindashboard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";   // 👈 imports App.jsx
+import App from "./App";   
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
